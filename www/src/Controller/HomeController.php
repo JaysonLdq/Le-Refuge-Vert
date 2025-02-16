27b208cf320efe9e05dc5d3ade4b64a53ce65568
JaysondLdq <jayson_34560@hotmail.fr>
2025-02-16 02:40:24 +0100

    ajout de la saison dynamique sur la page détail

--- www/src/Controller/HomeController.php
+++ www/src/Controller/HomeController.php
@@ -1,10 +1,7 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\Rental;
-use App\Entity\Saison;
-use App\Form\UserType;
 use App\Form\ProfilType;
 use App\Form\RentalType;
 use App\Repository\TarifRepository;
@@ -12,7 +9,6 @@ use App\Repository\RentalRepository;
 use App\Repository\SaisonRepository;
 use Symfony\Component\Form\FormError;
 use App\Repository\LogementRepository;
-use App\Repository\EquipementRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,7 +26,7 @@ class HomeController extends AbstractController
         EntityManagerInterface $em
     ): Response {
         // Récupérer la saison active (celle qui correspond à la période actuelle)
-        $saisonActuelle = $saisonRepository->findSeasonsActiveOnCurrentDate(); // Tu peux utiliser la méthode que tu as déjà dans le SaisonRepository
+        $saisonActuelle = $saisonRepository->findSeasonsActiveOnCurrentDate(); 
     
         if (!$saisonActuelle) {
             // Si aucune saison active n'est trouvée, définir une saison par défaut (par exemple Haute saison)
@@ -59,6 +55,18 @@ class HomeController extends AbstractController
         $logementsAvecPrix = array_map(function($logement) use ($tarifRepository, $saisonActuelle) {
             $tarif = $tarifRepository->findTarif($logement, $saisonActuelle);
             $price = $tarif ? $tarif->getPrice() : "Tarif indisponible";
+
+            // Si la saison est la haute saison, appliquer une augmentation de 20%
+        if ($saisonActuelle->getLabel() === 'Haute saison' && $tarif) {
+            $price = $tarif->getPrice() * 1.2;  // Augmenter de 20%
+        }
+
+            // Si la saison est la haute saison, appliquer une augmentation de 20%
+            if ($saisonActuelle->getLabel() === 'Basse saison' && $tarif) {
+                $price = number_format($tarif->getPrice() / 1.2, 2);
+            }
+
+
             return ['logement' => $logement, 'price' => $price];
         }, $logements);
     
@@ -69,106 +77,155 @@ class HomeController extends AbstractController
     }
     
 
+// Pour la démo et montrer que ca marche avec les autres saisons
+//  public function index(
+//     SaisonRepository $saisonRepository,
+//     LogementRepository $logementRepository,
+//     TarifRepository $tarifRepository
+// ): Response {
+//     // Récupération de toutes les saisons actives
+//     $saisonActuelle = $saisonRepository->findSeasonsActiveOnCurrentDate();
+
+//     // Débogage de la saison récupérée
+//     dump($saisonActuelle);  // Ceci doit afficher les saisons récupérées
+
+//     if (empty($saisonActuelle)) {
+//         // Si aucune saison n'est trouvée, on dit aucune saison actuelle
+//         $saisonActuelle = $saisonRepository->findOneBy(['label' => 'basse saison']);
+//     }
+
+//     // Récupérer tous les logements
+//     $logements = $logementRepository->findAll();
+
+    
+
+//     // Récupérer les tarifs pour chaque logement et la saison actuelle
+//     $logementsAvecPrix = array_map(function($logement) use ($tarifRepository, $saisonActuelle) {
+//         $tarif = $saisonActuelle ? $tarifRepository->findTarif($logement, $saisonActuelle) : null;
+//         $price = $tarif ? $tarif->getPrice() : "Tarif indisponible";
+//         return ['logement' => $logement, 'price' => $price];
+//     }, $logements);
+
+//     return $this->render('home/index.html.twig', [
+//         'saison' => $saisonActuelle,  // Affichage de la saison actuelle
+//         'logementsAvecPrix' => $logementsAvecPrix,
+//     ]);
+// }
+
     
 
 
-    #[Route('/logement/{id}', name: 'logement_detail', methods: ['GET', 'POST'])]
-    public function logementById(
-        LogementRepository $logementRepository,
-        TarifRepository $tarifRepository,
-        SaisonRepository $saisonRepository,
-        RentalRepository $rentalRepository,
-        EntityManagerInterface $em,
-        Request $request,
-        int $id
-    ): Response {
-        $logement = $logementRepository->find($id);
-        if (!$logement) {
-            throw $this->createNotFoundException('Logement non trouvé');
-        }
-    
-        // Récupérer la saison actuelle
-        $saisonActuelle = $saisonRepository->findSeason();
-        
-
-        // Trouver le tarif pour le logement et la saison actuelle
-        $tarif = $saisonActuelle ? $tarifRepository->findTarif($logement, $saisonActuelle) : null;
-        $pricePerNight = $tarif ? $tarif->getPrice() : 0;
-    
-        // Récupérer les équipements du logement
-        $equipements = $logement->getEquipements();
-    
-        // Créer une réservation
-        $reservation = new Rental();
-        $reservationForm = $this->createForm(RentalType::class, $reservation);
-        $reservationForm->handleRequest($request);
-    
-        // Initialisation des variables
-        $daysCount = 0;
-        $totalPrice = 0;
-        $error = null;
-    
-        // Calcul automatique du nombre de jours avant soumission
-        $dateStart = $reservation->getDateStart();
-        $dateEnd = $reservation->getDateEnd();
-    
-        if ($dateStart && $dateEnd && $dateEnd > $dateStart) {
-            $interval = $dateStart->diff($dateEnd);
-            $daysCount = $interval->days;
-            $totalPrice = $daysCount * $pricePerNight;
-        }
-    
-        // Gestion de la soumission du formulaire
-        if ($reservationForm->isSubmitted() && $reservationForm->isValid()) {
-            $user = $this->getUser();
-            if (!$user) {
-                $this->addFlash('error', 'Vous devez être connecté pour réserver.');
-                return $this->redirectToRoute('app_login');
-            }
-    
-            // Vérifier que les dates sont valides
-            if (!$dateStart || !$dateEnd || $dateEnd <= $dateStart) {
-                $reservationForm->addError(new FormError('Les dates sélectionnées ne sont pas valides.'));
-            } else {
-                // Vérifier si la période est déjà réservée
-                $existingReservations = $rentalRepository->createQueryBuilder('r')
-                    ->where('r.logement = :logement')
-                    ->andWhere('r.dateStart < :dateEnd')
-                    ->andWhere('r.dateEnd > :dateStart')
-                    ->setParameter('logement', $logement)
-                    ->setParameter('dateStart', $dateStart)
-                    ->setParameter('dateEnd', $dateEnd)
-                    ->getQuery()
-                    ->getResult();
-    
-                if (count($existingReservations) > 0) {
-                    $reservationForm->addError(new FormError('Cette période est déjà réservée. Veuillez choisir une autre date.'));
-                } else {
-                    // Enregistrement de la réservation
-                    $reservation->setUsers($user);
-                    $reservation->setLogement($logement);
-                    $em->persist($reservation);
-                    $em->flush();
-    
-                    return $this->redirectToRoute('reservation_page', ['id' => $reservation->getId()]);
-                }
-            }
-        }
-    
-        return $this->render('home/details.html.twig', [
-            'logement' => $logement,
-            'saison' => $saisonActuelle,
-            'price' => $pricePerNight,
-            'equipements' => $equipements, 
-            'nbDays' => $daysCount,
-            'totalPrice' => $totalPrice,
-            'reservationForm' => $reservationForm->createView(),
-            'error' => $error
-        ]);
+#[Route('/logement/{id}', name: 'logement_detail', methods: ['GET', 'POST'])]
+public function logementById(
+    LogementRepository $logementRepository,
+    TarifRepository $tarifRepository,
+    SaisonRepository $saisonRepository,
+    RentalRepository $rentalRepository,
+    EntityManagerInterface $em,
+    Request $request,
+    int $id
+): Response {
+    $logement = $logementRepository->find($id);
+    if (!$logement) {
+        throw $this->createNotFoundException('Logement non trouvé');
     }
 
-    #[Route('/reservation/{id}', name: 'reservation_page', methods: ['GET', 'POST'], requirements: ['id' => '\d+'])]
+    // Récupérer la saison active
+    $saisonActuelle = $saisonRepository->findSeasonsActiveOnCurrentDate();
 
+    if (!$saisonActuelle) {
+        // Si aucune saison active n'est trouvée, définir une saison par défaut
+        $saisonActuelle = $saisonRepository->findOneBy(['label' => 'Haute saison']);
+    }
+
+    // Trouver le tarif pour le logement et la saison actuelle
+    $tarif = $tarifRepository->findTarif($logement, $saisonActuelle);
+    $pricePerNight = $tarif ? $tarif->getPrice() : 0;
+
+    // Si la saison est la haute saison, appliquer une augmentation de 20%
+    if ($saisonActuelle->getLabel() === 'Haute saison' && $tarif) {
+        $pricePerNight = $tarif->getPrice() * 1.2;  // Augmenter de 20%
+    }
+
+    // Si la saison est la basse saison, appliquer une réduction de 20%
+    if ($saisonActuelle->getLabel() === 'Basse saison' && $tarif) {
+        $pricePerNight = number_format($tarif->getPrice() / 1.2, 2);  // Réduire de 20%
+    }
+
+    // Récupérer les équipements du logement
+    $equipements = $logement->getEquipements();
+
+    // Créer une réservation
+    $reservation = new Rental();
+    $reservationForm = $this->createForm(RentalType::class, $reservation);
+    $reservationForm->handleRequest($request);
+
+    // Initialisation des variables
+    $daysCount = 0;
+    $totalPrice = 0;
+    $error = null;
+
+    // Calcul automatique du nombre de jours avant soumission
+    $dateStart = $reservation->getDateStart();
+    $dateEnd = $reservation->getDateEnd();
+
+    if ($dateStart && $dateEnd && $dateEnd > $dateStart) {
+        $interval = $dateStart->diff($dateEnd);
+        $daysCount = $interval->days;
+        $totalPrice = $daysCount * $pricePerNight;
+    }
+
+    // Gestion de la soumission du formulaire
+    if ($reservationForm->isSubmitted() && $reservationForm->isValid()) {
+        $user = $this->getUser();
+        if (!$user) {
+            $this->addFlash('error', 'Vous devez être connecté pour réserver.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Vérifier que les dates sont valides
+        if (!$dateStart || !$dateEnd || $dateEnd <= $dateStart) {
+            $reservationForm->addError(new FormError('Les dates sélectionnées ne sont pas valides.'));
+        } else {
+            // Vérifier si la période est déjà réservée
+            $existingReservations = $rentalRepository->createQueryBuilder('r')
+                ->where('r.logement = :logement')
+                ->andWhere('r.dateStart < :dateEnd')
+                ->andWhere('r.dateEnd > :dateStart')
+                ->setParameter('logement', $logement)
+                ->setParameter('dateStart', $dateStart)
+                ->setParameter('dateEnd', $dateEnd)
+                ->getQuery()
+                ->getResult();
+
+            if (count($existingReservations) > 0) {
+                $reservationForm->addError(new FormError('Cette période est déjà réservée. Veuillez choisir une autre date.'));
+            } else {
+                // Enregistrement de la réservation
+                $reservation->setUsers($user);
+                $reservation->setLogement($logement);
+                $em->persist($reservation);
+                $em->flush();
+
+                return $this->redirectToRoute('reservation_page', ['id' => $reservation->getId()]);
+            }
+        }
+    }
+
+    return $this->render('home/details.html.twig', [
+        'logement' => $logement,
+        'saison' => $saisonActuelle,
+        'price' => $pricePerNight,
+        'equipements' => $equipements, 
+        'nbDays' => $daysCount,
+        'totalPrice' => $totalPrice,
+        'reservationForm' => $reservationForm->createView(),
+        'error' => $error
+    ]);
+}
+
+
+#[Route('/reservation/{id}', name: 'reservation_page', methods: ['GET', 'POST'], requirements: ['id' => '\d+'])]
 public function update(
     Request $request,
     RentalRepository $rentalRepository,
@@ -189,8 +246,12 @@ public function update(
 
     // Récupérer le logement et la saison actuelle
     $logement = $rental->getLogement();
-    $saisonActuelle = $saisonRepository->findSeason();
-   
+    $saisonActuelle = $saisonRepository->findSeasonsActiveOnCurrentDate();  // Ici, on récupère la saison actuelle
+
+    if (!$saisonActuelle) {
+        // Si aucune saison active n'est trouvée, définir une saison par défaut
+        $saisonActuelle = $saisonRepository->findOneBy(['label' => 'Haute saison']);
+    }
 
     // Récupérer le tarif
     $pricePerNight = 0;
@@ -199,6 +260,16 @@ public function update(
         if ($tarif) {
             $pricePerNight = $tarif->getPrice();
         }
+    }
+
+    // Appliquer l'ajustement de prix basé sur la saison
+    if ($saisonActuelle->getLabel() === 'Haute saison' && $tarif) {
+        $pricePerNight = $tarif->getPrice() * 1.2;  // Augmenter de 20% pour la haute saison
+    }
+
+    // Appliquer l'ajustement de prix pour la basse saison
+    if ($saisonActuelle->getLabel() === 'Basse saison' && $tarif) {
+        $pricePerNight = number_format($tarif->getPrice() / 1.2, 2);  // Réduire de 20% pour la basse saison
     }
 
     // Créer le formulaire
@@ -254,7 +325,6 @@ public function update(
 
                 // Ajouter un message de succès et rediriger vers la page d'accueil
                 $this->addFlash('success', 'Réservation confirmée avec succès!');
-               
             }
         }
     }
@@ -268,6 +338,7 @@ public function update(
         'error' => $error,
     ]);
 }
+
 
 #[Route('/profil', name: 'app_profil', methods: ['GET', 'POST'])]   
 public function profil(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
@@ -306,7 +377,9 @@ public function delete(Request $request, RentalRepository $rentalRepository, Ent
         $entityManager->remove($rental);
         $entityManager->flush();
 
-        return $this->redirectToRoute('logement_detail');
+           // Rediriger vers le détail du logement associé à la réservation
+           return $this->redirectToRoute('logement_detail', ['id' => $rental->getLogement()->getId()]);
+        
     }
 
     return $this->render('rental/delete.html.twig', [
